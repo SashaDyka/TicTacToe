842115745b50b3cc140dd1f8e63aaed9b19e2cd1
SashaDyka <beli.rothi2005@gmail.com>
2025-08-11 16:39:46 +0300

    feat: add variable wins and function for it

--- js/player.js
+++ js/player.js
@@ -1,10 +1,22 @@
 export default class Player {
   constructor(symbol) {
     this.symbol = symbol;
+    this.wins = 0;
   }
 
   getSymbol() {
     return this.symbol;
   }
+
+  getWins() {
+    return this.wins;
+    }
+
+  addWin() {
+    this.wins++;
+  }
+
+  resetWins() {
+    this.wins = 0;
+  }
 }
-/*add save wins*/
