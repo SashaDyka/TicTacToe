d509ce37f494f911664a6fe3db5f6a0eb9149f26
SashaDyka <beli.rothi2005@gmail.com>
2025-08-11 18:09:42 +0300

    fear: edit connection

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import Board from './board.js';
 import View from './view.js';
 import Player from './player.js';
-import Game from './game.js';
+import GameController from './game.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const elements = {
         boardElement: document.getElementById('gameBoard'),
-        statusText: document.querySelector('.status'),
+        statusText: document.getElementById('status'),
         chooseXBtn: document.getElementById('chooseX'),
         chooseOBtn: document.getElementById('chooseO'),
         undoBtn: document.getElementById('undoBtn'),
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         reduceBtn: document.getElementById('reduceBtn'),
         enlargeBtn: document.getElementById('enlargeBtn'),
     };
-    const game = new Game(elements);
+    const game = new GameController(elements);
     game.startGame(); 
 });
 
